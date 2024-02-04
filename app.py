7e720240cbd72b6dc321fc5a7be3e122acf4559b
KevWang13 <95865075+KevWang13@users.noreply.github.com>
2024-02-04 16:35:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
-from flask import Flask,render_template,request
+from flask import Flask, request, render_template
 import google.generativeai as palm
-
-palm.configure(api_key="AIzaSyChWryfMea0Z0LlqYQHq2JmGOe2pRFblwI")
-
-defaults = { 'model': "models/chat-bison-001"}
-
+palm.configure(api_key="")
+model = {"model":"models/chat-bison-001"}
 app = Flask(__name__)
-
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        t = request.form.get("txt")
-        r = palm.chat(**defaults,messages=t)
+        q = request.form.get("q")
+        print(q)
+        r = palm.chat(
+            **model,
+            messages = q
+        )
         return(render_template("index.html",result=r.last))
     else:
-        return(render_template("index.html",result="waiting"))
-
+        return(render_template("index.html", result="waiting for question............."))
 if __name__ == "__main__":
     app.run()
